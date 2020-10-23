64218d6c05848aa9eca19dd982ac7fbedaba6137
Bram Bogaerts <11702928@student.pxl.be>
2020-10-23 14:07:45 +0200

    Login-screen hotel guests

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { WelcomeScreenComponent } from './components/welcome-screen/welcome-screen.component';
+import { GuestScreenComponent } from './components/guest-screen/guest-screen.component';
+import { AdminScreenComponent } from './components/admin-screen/admin-screen.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeScreenComponent,
+    GuestScreenComponent,
+    AdminScreenComponent
   ],
   imports: [
     BrowserModule
