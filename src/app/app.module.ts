f90731a3c69c9e5effc95c6d5e9cf27c63a897b1
Bram Bogaerts <11702928@student.pxl.be>
2020-10-23 22:08:20 +0200

    Update login-screen hotel guests, routing, guestservice, statemanagerservice, token-interceptor-service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,13 @@ import { AppComponent } from './app.component';
 import { WelcomeScreenComponent } from './components/welcome-screen/welcome-screen.component';
 import { GuestScreenComponent } from './components/guest-screen/guest-screen.component';
 import { AdminScreenComponent } from './components/admin-screen/admin-screen.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {AppRoutingModule} from './routing/app-routing.module';
+import {HttpClientModule} from '@angular/common/http';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {GuestService} from './services/guest.service';
+import {StateManagerService} from './services/state-manager.service';
+import {TokenInterceptorService} from './services/token-interceptor.service';
 
 @NgModule({
   declarations: [
@@ -14,9 +21,18 @@ import { AdminScreenComponent } from './components/admin-screen/admin-screen.com
     AdminScreenComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
   ],
-  providers: [],
+  providers: [
+    GuestService,
+    StateManagerService,
+    TokenInterceptorService,
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
